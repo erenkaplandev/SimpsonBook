3b5e1256b56b3327163f382c40e83676d5b3b2f0
hellstabber <ek.xcodeproject@gmail.com>
2022-04-07 01:32:26 +0300

    Initial Commit

---
+++ SimpsonBook/Simpson.swift
@@ -0,0 +1,22 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by hellstabber on 6.04.2022.
+//
+
+import Foundation
+import UIKit
+
+class Simpson {
+    var name : String
+    var job  : String
+    var image = UIImage()
+    
+    
+    init(nameInit : String, jobInit : String, imageInit : UIImage) {
+        name = nameInit
+        job = jobInit
+        image = imageInit
+    }
+}
